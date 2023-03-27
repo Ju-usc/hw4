3bfce59a5062bcacceb84cc3a1cb22a132e0b684
root <jlee9721@usc.edu>
2023-03-27 05:36:40 -0700

    hw4 equal-paths commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -13,6 +13,24 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if(root == nullptr){
+        return true;
+    }
+    int lh = height(root->left);
+    int rh = height(root->right);
+    if(lh <= 1 && rh <= 1){
+        return true;
+    }
+    return height(root->left) == height(root->right);
+}
+int height(Node* root){
+    if(root == nullptr){
+        return 0;
+    }
+    int rh = height(root->left);
+    int lh = height(root->right);
 
+
+    return max(rh, lh) + 1;
 }
 
