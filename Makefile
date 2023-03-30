a1e5dd2628cc62c74da4cdfab40706acc4c9ee31
Juyoung Lee <jlee9721@usc.edu>
2023-03-29 23:02:27 -0700

    bst-test.cpp pushed for clean git status

--- Makefile
+++ Makefile
@@ -4,10 +4,13 @@ CXXFLAGS=-g -Wall -std=c++11
 #DEFS=-DDEBUG
 
 
-all: bst-test equal-paths-test
+all: bst-test equal-paths-test 
 
 bst-test: bst-test.cpp bst.h avlbst.h
 	$(CXX) $(CXXFLAGS) $(DEFS) $< -o $@
+
+# avl-test: avl-test.cpp bst.h avlbst.h
+# 	$(CXX) $(CXXFLAGS) $(DEFS) $< -o $@
 
 # Brute force recompile all files each time
 equal-paths-test: equal-paths-test.cpp equal-paths.cpp equal-paths.h
--- bst-test.cpp
+++ bst-test.cpp
@@ -3,46 +3,21 @@
 #include "bst.h"
 #include "avlbst.h"
 
+
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
-    // Binary Search Tree tests
-    BinarySearchTree<char,int> bt;
-    bt.insert(std::make_pair('a',1));
-    bt.insert(std::make_pair('b',2));
-    
-    cout << "Binary Search Tree contents:" << endl;
-    for(BinarySearchTree<char,int>::iterator it = bt.begin(); it != bt.end(); ++it) {
-        cout << it->first << " " << it->second << endl;
-    }
-    if(bt.find('b') != bt.end()) {
-        cout << "Found b" << endl;
-    }
-    else {
-        cout << "Did not find b" << endl;
-    }
-    cout << "Erasing b" << endl;
-    bt.remove('b');
+    BinarySearchTree<uint16_t, uint16_t> testTree;
 
-    // AVL Tree Tests
-    AVLTree<char,int> at;
-    at.insert(std::make_pair('a',1));
-    at.insert(std::make_pair('b',2));
-
-    cout << "\nAVLTree contents:" << endl;
-    for(AVLTree<char,int>::iterator it = at.begin(); it != at.end(); ++it) {
-        cout << it->first << " " << it->second << endl;
-    }
-    if(at.find('b') != at.end()) {
-        cout << "Found b" << endl;
-    }
-    else {
-        cout << "Did not find b" << endl;
-    }
-    cout << "Erasing b" << endl;
-    at.remove('b');
-
+	testTree.insert(std::make_pair(5, 8));
+	// testTree.insert(std::make_pair(3, 159));
+	// testTree.insert(std::make_pair(1, 9));
+	// testTree.insert(std::make_pair(3, 0x5a));
+	// testTree.insert(std::make_pair(15, 0xb));
+	// testTree.insert(std::make_pair(1, 4));
+	testTree.insert(std::make_pair(5, 159));
+	testTree.print();
     return 0;
 }
